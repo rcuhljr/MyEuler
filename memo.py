c196c2db30983725667631eafb4a50a728bb834d
Robert C. Uhl <rcuhl@sep.com>
2012-01-11 11:51:08 -0500

    problem 2 solved, optimizing a little.

---
+++ memo.py
@@ -0,0 +1,8 @@
+def memo(func):
+    cache = {}
+    @ wraps(func)
+    def wrap(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrap
