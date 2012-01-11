4eff77c42c127942e7726d0d768bb257878c759c
Robert C. Uhl <rcuhl@sep.com>
2012-01-11 13:35:59 -0500

    problem 3, stupid fix removed two orders of magnitude pain.

--- prob3.py
+++ prob3.py
@@ -1,13 +1,17 @@
 import math
+import cProfile
 primes = [2]
 
 def fillPrimesFor(x):
     map(pumpPrime, range(3,math.trunc(math.sqrt(x)),2))
     
 def pumpPrime(x):
+    cutoff = math.sqrt(x)
     for n in primes:
         if x%n == 0:
-            return        
+            return 
+        elif n > cutoff:
+            break
     primes.append(x)        
         
 def solve(x):
@@ -19,6 +23,6 @@ def solve(x):
     print biggestFactor    
 
 
-solve(600851475143)
+print cProfile.run('solve(600851475143)')
 
 #going to attempt to speed this up with a better prime seive.
