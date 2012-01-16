2721964bcc563e04795cb25b2154740e34d64d84
Robert C. Uhl <rcuhl@sep.com>
2012-01-16 15:49:11 -0500

    problem 11.

---
+++ prob11.py
@@ -0,0 +1,48 @@
+import math
+import cProfile
+   
+def solve(file):
+    grid = loadGrid(file)     
+    m, n = len(grid[0]), len(grid)
+    maxval = 0
+    for x in range(m):
+        for y in range(n):
+            maxval = max(maxval, checkNeighbors(x, y, grid))
+    print maxval
+
+def checkNeighbors(x, y, grid):
+    xvals = [ 1, 1, 1, 0]    
+    yvals = [-1, 0, 1, 1]
+    m, n = len(grid[0]), len(grid)    
+    def checkdir(xstep, ystep):
+        if(xstep > 0 and x+3 >= m):
+            return 0
+        if(ystep > 0 and y+3 >= n):
+            return 0
+        if(ystep < 0 and y-3 < 0):
+            return 0        
+        return int(grid[y][x])*int(grid[y+ystep][x+xstep])*int(grid[y+ystep*2][x+xstep*2])*int(grid[y+ystep*3][x+xstep*3])
+    return max(map(checkdir, xvals, yvals))
+    
+def loadGrid(file):
+    result = []
+    fin = open(file, 'rb')
+    for line in fin:
+        result.append(line.rstrip().split(' '))
+    return result    
+
+print cProfile.run('solve(\'prob11.txt\')')
+
+#initial thoughts, move through the grid right to left, top to bottom, checking 4 down, 4 right, and 8 diagonal (up and down right) as grid bounds permit
+#this will hit all the possible combinations without any duplication I believe.
+# 0 = grid space X = check, T = current square.
+#   OOOOOOOXO
+#   OOOOOOXOO
+#   OOOOOXOOO
+#   OOOOTXXXO
+#   OOOOXXOOO
+#   OOOOXOXOO
+#   OOOOXOOXO
+
+
+
