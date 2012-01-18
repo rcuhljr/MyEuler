9f0a11babb587f870d4618f89cae8d38191c3be1
Robert C. Uhl <rcuhl@sep.com>
2012-01-18 14:34:32 -0500

    problem 18 and 67 are the same thing.

---
+++ prob67.py
@@ -0,0 +1,32 @@
+import math
+import cProfile
+   
+def solve(file):
+    grid = loadGrid(file)
+    grid.reverse()
+    previousRow = [0 for i in range(len(grid[0]))]
+    for row in grid:
+        width = len(row)
+        newRow = []
+        if width > 1:
+            for n in range(width-1):                
+                if(row[n]+previousRow[n] > row[n+1]+previousRow[n+1]):
+                    newRow.append(row[n]+previousRow[n])
+                else:
+                    newRow.append(row[n+1]+previousRow[n+1])
+            previousRow = newRow        
+        else:            
+            print row[0]+previousRow[0]
+    
+
+def loadGrid(file):
+    result = []
+    fin = open(file, 'rb')
+    for line in fin:
+        result.append(map(lambda x: int(x),line.rstrip().split(" ")))
+    return result    
+
+print cProfile.run('solve(\'triangle.txt\')')
+
+
+#solving this one while I'm here since it's the same problem.
